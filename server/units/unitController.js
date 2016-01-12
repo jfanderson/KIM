c0a3467658cc5f0ff5d2bb71b7fda77389520938
John Anderson <jfanderson373@gmail.com>
2016-01-11 22:07:12 -0800

    Add routes and CRUD methods for material units

---
+++ server/units/unitController.js
@@ -0,0 +1,39 @@
+var models = require('../db/index.js');
+var MaterialUnit = models.MaterialUnit;
+
+module.exports = {
+
+  getMaterialUnits: function(req, res) {
+    MaterialUnit.findAll().then(function(units) {
+      res.status(200).send({ units: units });
+    }).catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  },
+
+  addMaterialUnit: function(req, res) {
+    MaterialUnit.create(req.body.unit).then(function(newUnit) {
+      res.status(201).send({ unit: newUnit });
+    }).catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  },
+
+  removeMaterialUnit: function(req, res) {
+    MaterialUnit.findOne({ where: { unit: req.params.unit }}).then(function(unit) {
+      if (unit === null) {
+        res.sendStatus(404);
+      } else {
+        return unit.destroy();
+      }
+    }).then(function() {
+      res.sendStatus(204);
+    }).catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  },
+
+};
