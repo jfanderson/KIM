73cc37516150f130e8d59b68b6346243fca9aaeb
John Anderson <jfanderson373@gmail.com>
2016-01-18 16:28:50 -0800

    Add functions for associating materials with pieces

--- server/pieces/pieceController.js
+++ server/pieces/pieceController.js
@@ -1,100 +1,159 @@
 var models = require('../db/index.js');
 var Piece = models.Piece;
 var PieceType = models.PieceType;
+var Material = models.Material;
+var PieceMaterial = models.PieceMaterial;
 
 module.exports = {
+  getPiece: getPiece,
+  getAllPieces: getAllPieces,
+  addPiece: addPiece,
+  modifyPiece: modifyPiece,
+  removePiece: removePiece,
+  linkMaterial: linkMaterial,
+  modifyMaterialQty: modifyMaterialQty,
+  unlinkMaterial: unlinkMaterial
+};
 
-  getPiece: function(req, res) {
-    Piece.findById(req.params.pieceId).then(function(piece) {
-      if (piece === null) {
-        res.sendStatus(404);
-      } else {
-        res.status(200).send({ piece: piece });
-      }
-    }).catch(function(error) {
-      console.log(error);
-      res.sendStatus(500);
-    });
-  },
-
-  getAllPieces: function(req, res) {
-    Piece.findAll().then(function(pieces) {
-      res.status(200).send({ pieces: pieces });
-    }).catch(function(error) {
-      console.log(error);
-      res.sendStatus(500);
-    });
-  },
-
-  addPiece: function(req, res) {
-    // save type to associate later
-    if (req.body.piece.hasOwnProperty('type')) {
-      var type = req.body.piece.type;
-      delete req.body.piece.type;
+function getPiece(req, res) {
+  Piece.findById(req.params.pieceId).then(function(piece) {
+    if (piece === null) {
+      res.sendStatus(404);
+    } else {
+      res.status(200).send({ piece: piece });
     }
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
 
-    Piece.create(req.body.piece).then(function(piece) {
-      // associate type if given
-      if (type) {
-        PieceType.findOne({ where: { name: type }}).then(function(matchedType) {
-          if (matchedType !== null) {
-            piece.typeId = matchedType.id;
-          }
-          res.status(201).send({ piece: piece });
-        });
-      } else {
+function getAllPieces(req, res) {
+  Piece.findAll().then(function(pieces) {
+    res.status(200).send({ pieces: pieces });
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
+
+function addPiece(req, res) {
+  // save type to associate later
+  if (req.body.piece.hasOwnProperty('type')) {
+    var type = req.body.piece.type;
+    delete req.body.piece.type;
+  }
+
+  Piece.create(req.body.piece).then(function(piece) {
+    // associate type if given
+    if (type) {
+      PieceType.findOne({ where: { name: type }}).then(function(matchedType) {
+        if (matchedType !== null) {
+          piece.typeId = matchedType.id;
+        }
         res.status(201).send({ piece: piece });
-      }
-    }).catch(function(error) {
-      console.log(error);
-      if (error.errors[0].message === 'item must be unique') {
-        res.sendStatus(409);
-      } else {
-        res.sendStatus(500);
-      }
-    });
-  },
+      });
+    } else {
+      res.status(201).send({ piece: piece });
+    }
+  }).catch(function(error) {
+    console.log(error);
+    if (error.errors[0].message === 'item must be unique') {
+      res.sendStatus(409);
+    } else {
+      res.sendStatus(500);
+    }
+  });
+}
 
-  modifyPiece: function(req, res) {
-    Piece.findById(req.params.pieceId).then(function(piece) {
-      if (piece === null) {
-        res.sendStatus(404);
-      } else if (req.body.hasOwnProperty('type')) {
-        // modify type if necessary
-        PieceType.findOne({ where: { name: req.body.type }}).then(function(matchedType) {
-          if (matchedType !== null) {
-            piece.typeId = matchedType.id;
-          }
-          delete req.body.type;
+function modifyPiece(req, res) {
+  Piece.findById(req.params.pieceId).then(function(piece) {
+    if (piece === null) {
+      res.sendStatus(404);
+    } else if (req.body.hasOwnProperty('type')) {
+      // modify type if necessary
+      PieceType.findOne({ where: { name: req.body.type }}).then(function(matchedType) {
+        if (matchedType !== null) {
+          piece.typeId = matchedType.id;
+        }
+        delete req.body.type;
 
-          piece.update(req.body).then(function(updatedPiece) {
-            res.status(200).send({ piece: updatedPiece });
-          });
-        });
-      } else {
         piece.update(req.body).then(function(updatedPiece) {
           res.status(200).send({ piece: updatedPiece });
         });
-      }
-    }).catch(function(error) {
-      console.log(error);
-      res.sendStatus(500);
-    });
-  },
+      });
+    } else {
+      piece.update(req.body).then(function(updatedPiece) {
+        res.status(200).send({ piece: updatedPiece });
+      });
+    }
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
 
-  removePiece: function(req, res) {
-    Piece.findById(req.params.pieceId).then(function(piece) {
-      if (piece === null) {
-        res.sendStatus(404);
-      } else {
-        return piece.destroy();
-      }
-    }).then(function() {
+function removePiece(req, res) {
+  Piece.findById(req.params.pieceId).then(function(piece) {
+    if (piece === null) {
+      res.sendStatus(404);
+    } else {
+      return piece.destroy();
+    }
+  }).then(function() {
+    res.sendStatus(204);
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
+
+function linkMaterial(req, res) {
+  Piece.findById(req.params.pieceId).then(function(piece) {
+    if (piece === null) {
+      res.sendStatus(404);
+    }
+
+    return piece.addMaterial(req.params.materialId).then(function() {
+      res.sendStatus(200);
+    });
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
+
+function modifyMaterialQty(req, res) {
+  PieceMaterial.findOne({
+    where: {
+      pieceId: req.params.pieceId,
+      materialId: req.params.materialId
+    }
+  }).then(function(result) {
+    if (result === null) {
+      res.sendStatus(404);
+    }
+
+    return result.update({ qty: req.body.qty }).then(function() {
+      res.sendStatus(200);
+    });
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
+
+function unlinkMaterial(req, res) {
+  Piece.findById(req.params.pieceId).then(function(piece) {
+    if (piece === null) {
+      res.sendStatus(404);
+    }
+
+    return piece.removeMaterial(req.params.materialId).then(function() {
       res.sendStatus(204);
-    }).catch(function(error) {
-      console.log(error);
-      res.sendStatus(500);
     });
-  }
-
-};
+  }).catch(function(error) {
+    console.log(error);
+    res.sendStatus(500);
+  });
+}
