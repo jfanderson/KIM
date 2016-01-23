1493ba084533659d11ee60cdb636f0efd61bd69e
John Anderson <jfanderson373@gmail.com>
2016-01-22 22:36:32 -0800

    Set up front end build system

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 // serve static files
-// app.use(express.static(__dirname + '/../../client'));
+app.use(express.static(__dirname + '/../client'));
 
 // all requests routed in middleware.js
 require('./config/middleware.js')(app, express);
