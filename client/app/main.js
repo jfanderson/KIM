2b34f497c27777a580011897bbb29906b9e297ca
John Anderson <jfanderson373@gmail.com>
2016-01-24 10:54:48 -0800

    Set up react routes and navbar

--- client/app/main.js
+++ client/app/main.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>Hello KIM!</div>
-    );
-  }
-}
+import App from './components/App.js';
+import JewelryProducts from './components/JewelryProducts.js';
+import Materials from './components/Materials.js';
+import OtherProducts from './components/OtherProducts.js';
+import PurchaseOrders from './components/PurchaseOrders.js';
+import Sales from './components/Sales.js';
+import Vendors from './components/Vendors.js';
+import Settings from './components/Settings.js';
+import NoMatch from './components/NoMatch.js';
 
-ReactDOM.render(<App/>, document.getElementById('main'));
+const history = createBrowserHistory();
+
+ReactDOM.render((
+  <Router history={history}>
+    <Route path='/' component={App}>
+      <Route path='jewelry' component={JewelryProducts}/>
+      <Route path='materials' component={Materials}/>
+      <Route path='products' component={OtherProducts}/>
+      <Route path='purchaseorders' component={PurchaseOrders}/>
+      <Route path='sales' component={Sales}/>
+      <Route path='vendors' component={Vendors}/>
+      <Route path='settings' component={Settings}/>
+    </Route>
+    <Route path='*' component={NoMatch}/>
+  </Router>
+), document.getElementById('main'));
