492ebe8600fb77b3cb35da0479f84eeb1ff01dc8
John Anderson <jfanderson373@gmail.com>
2016-01-24 22:00:39 -0800

    Display data with a Table component

--- client/app/components/JewelryProducts.js
+++ client/app/components/JewelryProducts.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import 'whatwg-fetch';
-
 import h from '../helpers.js';
+
+import Table from './Table.js';
+import Column from './Column.js';
+import Cell from './Cell.js';
 
 class JewelryProducts extends React.Component {
   constructor() {
@@ -31,17 +34,16 @@ class JewelryProducts extends React.Component {
     return (
       <div>
         <h1>Jewelry Products</h1>
-        <ul>
-          {this.state.pieces.map(piece => {
-            return (
-              <li>
-                <h5>{piece.description}</h5>
-                <div>${piece.msrp}</div>
-                <div>Qty: {piece.qtyInStock}</div>
-              </li>
-            );
-          })}
-        </ul>
+        <Table data={this.state.pieces} uniqueId="item">
+          <Column header="Item #" cell={piece => ( <Cell>{piece.item}</Cell> )}/>
+          <Column header="Description" cell={piece => ( <Cell>{piece.description}</Cell> )}/>
+          <Column header="Type" cell={piece => ( <Cell></Cell> )}/>
+          <Column header="Cost" cell={piece => ( <Cell>{piece.totalCost}</Cell> )}/>
+          <Column header="Wholesale" cell={piece => ( <Cell>{piece.wholesalePrice}</Cell> )}/>
+          <Column header="MSRP" cell={piece => ( <Cell>{piece.msrp}</Cell> )}/>
+          <Column header="Qty on Order" cell={piece => ( <Cell>{piece.qtyOnOrder}</Cell> )}/>
+          <Column header="Qty in Stock" cell={piece => ( <Cell>{piece.qtyInStock}</Cell> )}/>
+        </Table>
       </div>
     );
   }
