cead8cea437baeb00da3740f5ec42b9d278041c3
John Anderson <jfanderson373@gmail.com>
2016-02-03 00:06:08 -0800

    Make table fields editable

--- client/app/components/Cell.js
+++ client/app/components/Cell.js
@@ -1,13 +1,76 @@
 import React from 'react';
 
+const PropTypes = React.PropTypes;
+
 class Cell extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      editing: false,
+      value: ''
+    };
+  }
+
+  _handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  _handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      this.props.modifyField(this.state.value);
+      this.setState({ editing: false });
+    } else if (event.keyCode === 27) {
+      this.setState({ editing: false });
+    }
+  }
+
+  _startEditing() {
+    this.setState({
+      editing: true,
+      value: this.props.children
+    });
+
+    setTimeout(() => {
+      this.refs.inputField.select();
+    }, 0);
+  }
+
   render() {
-    return (
-      <td>
-        {this.props.children}
-      </td>
-    );
+    let props = this.props;
+    let state = this.state;
+
+    if (props.modifyField && state.editing) {
+      return (
+        <td>
+          <input autoFocus type="text"
+            ref="inputField"
+            value={state.value}
+            onKeyDown={this._handleKeyDown.bind(this)}
+            onChange={this._handleChange.bind(this)}
+            />
+        </td>
+      );
+    } else if (props.modifyField) {
+      return (
+        <td onClick={this._startEditing.bind(this)}>
+          {props.children}
+        </td>
+      );
+    } else {
+      return (
+        <td>
+          {props.children}
+        </td>
+      );
+    }
   }
 }
+
+Cell.propTypes = {
+  // If passed, make cell editable on click
+  // Callback invoked with new cell value
+  modifyField: PropTypes.func
+};
 
 export default Cell;
--- client/app/components/JewelryProduct.js
+++ client/app/components/JewelryProduct.js
@@ -20,7 +20,7 @@ class JewelryProduct extends React.Component {
       .then(h.checkStatus)
       .then(h.parseJSON)
       .then(data => {
-        this.setState({ piece: data.piece });
+        this.setState({ piece: [data.piece] });
       })
       .catch(error => {
         console.log('Error fetching piece: ', error);
@@ -30,20 +30,42 @@ class JewelryProduct extends React.Component {
       });
   }
 
-  render() {
-    // pass piece as array
-    let pieceArray = [this.state.piece];
+  _modifyField(field, value) {
+    this.state.piece[0][field] = value;
 
+    this.setState({ piece: this.state.piece });
+
+    console.log(this.state);
+    // post to api
+  }
+
+  render() {
     return (
-      <Table classes="piece" data={ pieceArray } uniqueId="item">
-        <Column header="Item #" cell={piece => ( <Cell>{piece.item}</Cell> )}/>
-        <Column header="Description" cell={piece => ( <Cell>{piece.description}</Cell> )}/>
-        <Column header="Type" cell={piece => ( <Cell></Cell> )}/>
-        <Column header="Cost" cell={piece => ( <Cell>{piece.totalCost}</Cell> )}/>
-        <Column header="Wholesale" cell={piece => ( <Cell>{piece.wholesalePrice}</Cell> )}/>
-        <Column header="MSRP" cell={piece => ( <Cell>{piece.msrp}</Cell> )}/>
-        <Column header="Qty on Order" cell={piece => ( <Cell>{piece.qtyOnOrder}</Cell> )}/>
-        <Column header="Qty in Stock" cell={piece => ( <Cell>{piece.qtyInStock}</Cell> )}/>
+      <Table classes="piece" data={ this.state.piece } uniqueId="item">
+        <Column header="Item #" cell={piece => (
+          <Cell modifyField={value => this._modifyField('item', value)}>{piece.item}</Cell>
+        )}/>
+        <Column header="Description" cell={piece => (
+          <Cell modifyField={value => this._modifyField('description', value)}>{piece.description}</Cell>
+        )}/>
+        <Column header="Type" cell={piece => (
+          <Cell modifyField={value => this._modifyField('type', value)}></Cell>
+        )}/>
+        <Column header="Cost" cell={piece => (
+          <Cell modifyField={value => this._modifyField('totalCost', value)}>{'$' + piece.totalCost}</Cell>
+        )}/>
+        <Column header="Wholesale" cell={piece => (
+          <Cell modifyField={value => this._modifyField('wholesalePrice', value)}>{'$' + piece.wholesalePrice}</Cell>
+        )}/>
+        <Column header="MSRP" cell={piece => (
+          <Cell modifyField={value => this._modifyField('msrp', value)}>{'$' + piece.msrp}</Cell>
+        )}/>
+        <Column header="Qty on Order" cell={piece => (
+          <Cell modifyField={value => this._modifyField('qtyOnOrder', value)}>{piece.qtyOnOrder}</Cell>
+        )}/>
+        <Column header="Qty in Stock" cell={piece => (
+          <Cell modifyField={value => this._modifyField('qtyInStock', value)}>{piece.qtyInStock}</Cell>
+        )}/>
       </Table>
 
     );
