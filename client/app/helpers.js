dcad34dc5867e31bd7600f5d3434b82c129fa14e
John Anderson <jfanderson373@gmail.com>
2016-02-24 00:43:40 -0800

    Add findTypeName, findTypeId, and capitalize functions

--- client/app/helpers.js
+++ client/app/helpers.js
@@ -1,14 +1,24 @@
 import 'whatwg-fetch';
+import _ from 'underscore';
 
 var helpers = {
+  capitalize,
   checkStatus,
   displayPrice,
+  findTypeId,
+  findTypeName,
   parseJSON,
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json'
   }
 };
+
+function capitalize(string) {
+  if (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
 
 function checkStatus(res) {
   if (res.status >= 200 && res.status < 300) {
@@ -30,6 +40,16 @@ function displayPrice(price) {
   } else {
     return '$' + price.toFixed(2);
   }
+}
+
+function findTypeId(types, name) {
+  let type = _.findWhere(types, { name: name });
+  return type && type.id;
+}
+
+function findTypeName(types, id) {
+  let type = _.findWhere(types, { id: id });
+  return type && type.name;
 }
 
 function parseJSON(res) {
